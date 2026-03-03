bc3fa1951a16d1a1e66b4f68c7c7204e15484818
mdtuhin40101-dotcom <mdtuhin40101@gmail.com>
2026-03-03 16:19:20 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,25 @@
+import telebot
+import os
+from flask import Flask
+import threading
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Bot is running successfully!")
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host="0.0.0.0", port=10000)
+
+threading.Thread(target=run).start()
+
+bot.infinity_polling()
